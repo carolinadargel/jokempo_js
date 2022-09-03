7681f4f2209489c433284e9d50c184e3829f8dba
Your Name <carolinadargel@Carolinas-Mac-mini.local>
2022-09-03 14:18:58 -0300

    adding brackets

--- script.js
+++ script.js
@@ -18,27 +18,41 @@ function computerPlay() {
     let choices = [CHOICE.rock, CHOICE.paper, CHOICE.scissors];
     let randomizeChoice = Math.floor(Math.random() * 3);
     return choices[randomizeChoice]
-}    
+};    
 
 
 //Setting the results of what beats what based on user's and computer's choices
-function playRound(playerSelection, computerSelection) {   
+function playRound(playerSelection, computerSelection) {  
+
     let player1 = playerSelection;
     let player2 = computerSelection;
     let roundResult;    
-    if (player1 === player2) roundResult = RESULT.tie;
-    else if (player1 === CHOICE.rock && player2 === CHOICE.scissors ||
+
+    let player1Win = player1 === CHOICE.rock && player2 === CHOICE.scissors ||
         player1 === CHOICE.paper && player2 === CHOICE.rock ||
-        player1 === CHOICE.scissors && player2 === CHOICE.paper) roundResult = RESULT.player1;           
-    else if (player1 === CHOICE.rock && player2 === CHOICE.paper ||
-            player1 === CHOICE.paper && player2 === CHOICE.scissors ||
-            player1 === CHOICE.scissor && player2 === CHOICE.rock) roundResult = RESULT.player2;
+        player1 === CHOICE.scissors && player2 === CHOICE.paper;
+    
+    let player2Win = player1 === CHOICE.rock && player2 === CHOICE.paper ||
+        player1 === CHOICE.paper && player2 === CHOICE.scissors ||
+        player1 === CHOICE.scissor && player2 === CHOICE.rock;
+   
+    if (player1 === player2) {
+        roundResult = RESULT.tie;
+    
+    } else if (player1Win) {
+        roundResult = RESULT.player1;           
+    
+    } else if (player2Win) {
+        roundResult = RESULT.player2;
+    }
+
     return [roundResult, player1, player2]
 };
 
 
 //Run 5 rounds and save score
 function game(){       
+
     let player1Choice;
     let player2Choice;
     let roundResult;
@@ -61,34 +75,56 @@ function game(){
     }                
 };
 
+
 //print each round
 function printRound(i, roundResult0, roundResult1, roundResult2, player1Score, player2Score, tieScore){       
+    
     console.log(`ROUND #${i + 1}`);
     console.log(`Your Selection: ${roundResult1}`);
     console.log(`Computers Selection: ${roundResult2}`);
-    if (roundResult0 == RESULT.player1) console.log(`-----> You win! ${roundResult1} beats ${roundResult2} <-----`);
-    else if (roundResult0 == RESULT.player2) console.log(`-----> You lose! ${roundResult2} beats ${roundResult1} <-----`);
-    else if (roundResult0 == RESULT.tie) console.log(`-----> Both players have chosen ${roundResult1}. It's a tie! <-----`);
+
+    if (roundResult0 == RESULT.player1) {
+        console.log(`-----> You win! ${roundResult1} beats ${roundResult2} <-----`);
+
+    } else if (roundResult0 == RESULT.player2) {
+        console.log(`-----> You lose! ${roundResult2} beats ${roundResult1} <-----`);
+    
+    } else if (roundResult0 == RESULT.tie) {
+        console.log(`-----> Both players have chosen ${roundResult1}. It's a tie! <-----`);
+    }
+
     console.log(`Your score: ${player1Score}`);
     console.log(`Computer's score: ${player2Score}`);
     console.log(`Draw: ${tieScore}`);
+    
     if (i === 4){
         return finalScore(player1Score, player2Score, tieScore);
     }
+
     console.log(DIV1);
 };
 
 //print final result after 5 rounds
 function finalScore(player1FinalScore, player2FinalScore, drawScore) {
+
     console.log(DIV2);
     console.log('FINAL SCORE');
     console.log(`Your score: ${player1FinalScore}`);
     console.log(`Computer's score: ${player2FinalScore}`);
     console.log(`Draws: ${drawScore}`);
-    if (player1FinalScore > player2FinalScore) console.log(`Congratulations, you win! You rock!`);   
-    else if (player2FinalScore > player1FinalScore) console.log(`Bummer... You lost! Maybe next time. ;)`);   
-    else if (player2FinalScore == player1FinalScore) console.log(`You and the computer are tied. Try again!`);
+
+    if (player1FinalScore > player2FinalScore) {
+        console.log(`Congratulations, you win! You rock!`);   
+    
+    } else if (player2FinalScore > player1FinalScore) {
+        console.log(`Bummer... You lost! Maybe next time. ;)`);   
+    
+    } else if (player2FinalScore == player1FinalScore) {
+        console.log(`You and the computer are tied. Try again!`);
+    }
+
     console.log(DIV2);
+    
 };
 
 alert('Ready to play papper, rock & scissors? Tip: check your console');
